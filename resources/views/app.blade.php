02c05a1b8b59ebe54897739ccaa5e55e5324b808
rinofahreza <rinofahreza@gmail.com>
2026-01-02 16:00:16 +0700

    chore: Configure PWA native feel (disable zoom, selection, touch-callout)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover">
     <meta name="google" content="notranslate">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
@@ -32,7 +33,9 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Dancing+Script:wght@400;700&family=Great+Vibes&family=Satisfy&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Pacifico&family=Dancing+Script:wght@400;700&family=Great+Vibes&family=Satisfy&display=swap"
+        rel="stylesheet">
 
     <!-- Scripts -->
     @routes
@@ -52,7 +55,7 @@
                         console.log('ServiceWorker registration failed:', error);
                     });
             });
-        }
+    }
     </script>
 </head>
 
